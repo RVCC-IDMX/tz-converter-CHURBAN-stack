310e43d0e78bce5a63d6ab9206368801f6db397d
Christopher hurban <54687117+CHURBAN-stack@users.noreply.github.com>
2021-10-13 19:52:54 -0400

    Do changes to file

--- tz.js
+++ tz.js
@@ -1,8 +1,18 @@
-/* eslint-disable no-unused-vars */
-const moment = require ('moment-timezone');
+const moment = require('moment-timezone');
+const yargs = require('yargs');
+const FORMAT = 'dddd, MMMM Do YYYY, h:mm:ss a';
 
 moment.tz.setDefault('America/New_York');
 
-const targetTimezone = "Europe/Paris";
+const targetTimezone = yargs.argv._[0]; 
 
-moment()
+if (! targetTimezone ) {
+    console.log('Usage: node tz <timezone> [--format]');
+    process.exit(1);
+  }
+const params = yargs.argv;
+if (params.format === true){
+    formating = FORMAT; 
+}
+console.log(`The time at the ${'America/New_York'} timezone is ${moment().tz('America/New_York').format()}`);
+console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
